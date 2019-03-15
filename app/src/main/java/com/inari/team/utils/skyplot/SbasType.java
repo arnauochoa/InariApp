778dc2528d4c4f1109ed17e65dba1889ec607322
albertbecervas <albertbecerrahervas@gmail.com>
2019-03-15 08:08:07 +0100

    add some skyplot classes

---
+++ app/src/main/java/com/inari/team/utils/skyplot/SbasType.java
@@ -0,0 +1,9 @@
+package com.inari.team.utils.skyplot;
+
+/**
+ * Types of Satellite Based Augmentation Systems. See
+ * https://gssc.esa.int/navipedia/index.php/SBAS_Systems.
+ */
+public enum SbasType {
+    WAAS, EGNOS, GAGAN, MSAS, SDCM, SNAS, SACCSA, UNKNOWN
+}
