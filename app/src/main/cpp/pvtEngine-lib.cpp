29cb72f1f09f6a85a3230894e2caaf2ddc0008cb
arnauochoa <arnau.ochoa@gmail.com>
2019-03-23 20:00:01 +0100

    remove mode color and name

--- app/src/main/cpp/pvtEngine-lib.cpp
+++ app/src/main/cpp/pvtEngine-lib.cpp
@@ -16,44 +16,43 @@ Java_com_inari_team_presentation_ui_position_PositionViewModel_obtainPosition(JN
                                                                               jstring jsonData) {
 
     const char *gnssData = env->GetStringUTFChars(jsonData, nullptr);
-
-    // Read json
-    json gnssInfo = json::parse(gnssData);
-    json firstGnssInfo = gnssInfo[2];
-
-    // Create aquisistion
-    Info acqInfo;
-    extract_info(firstGnssInfo, acqInfo);
-
-    // eph Matrix
-    double **eph;
-    double *iono;
-    int ephN, ephM;
-    getEphMatrix(acqInfo, eph, iono, ephN, ephM);
-
-    // Some initializations
-
-    // Number of iterations used to obtain the PVT solution
-    int Nit = 5;
-    // Reference position (check RINEX file or website of the station)
-    // int PVTr = acq_info.refLocation.XYZ;   // FIXME: add reference time
-    // Preliminary guess for PVT solution
-    double PVT0[4];
-    //TODO: get preliminay guess, from obs header?
-    PVT0[0] = acqInfo.RefLocation.XYZ.x;
-    PVT0[1] = acqInfo.RefLocation.XYZ.y;
-    PVT0[2] = acqInfo.RefLocation.XYZ.z;
-
-    bool enabCorr = true;
-
-    double PVT[4];    //  PVT solution
-    double posllh[3];      //  Position in Latitude, Longitude and Height
-
     std::string lat;
     std::string lng;
 
-    try
-    {
+    // Read json
+    try {
+        json gnssInfo = json::parse(gnssData);
+        json firstGnssInfo = gnssInfo[2];
+
+        // Create aquisistion
+        Info acqInfo;
+        extract_info(firstGnssInfo, acqInfo);
+
+        // eph Matrix
+        double **eph;
+        double *iono;
+        int ephN, ephM;
+        getEphMatrix(acqInfo, eph, iono, ephN, ephM);
+
+        // Some initializations
+
+        // Number of iterations used to obtain the PVT solution
+        int Nit = 5;
+        // Reference position (check RINEX file or website of the station)
+        // int PVTr = acq_info.refLocation.XYZ;   // FIXME: add reference time
+        // Preliminary guess for PVT solution
+        double PVT0[4];
+        //TODO: get preliminay guess, from obs header?
+        PVT0[0] = acqInfo.RefLocation.XYZ.x;
+        PVT0[1] = acqInfo.RefLocation.XYZ.y;
+        PVT0[2] = acqInfo.RefLocation.XYZ.z;
+
+        bool enabCorr = true;
+
+        double PVT[4];    //  PVT solution
+        double posllh[3];      //  Position in Latitude, Longitude and Height
+
+
         PVT_recls(acqInfo, eph, ephN, ephM, iono, Nit, PVT0, enabCorr, PVT);
         xyz2llh(PVT, posllh);     // Getting position in Latitude, Longitude, Height format
 
@@ -70,8 +69,8 @@ Java_com_inari_team_presentation_ui_position_PositionViewModel_obtainPosition(JN
     }
     catch(const std::exception& e)
     {
-        lat = "";
-        lng = "";
+        lat = "null";
+        lng = "null";
     }
 
     std::string position = "{ \"lat\":" + lat + ", \"lng\": " + lng + " }";
