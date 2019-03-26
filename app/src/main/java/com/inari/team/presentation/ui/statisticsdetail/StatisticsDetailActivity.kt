c4e5de2e43eb1529e2ab14efffa60d7451bc38b2
arnauochoa <arnau.ochoa@gmail.com>
2019-03-26 23:34:52 +0100

    control commit

--- app/src/main/java/com/inari/team/presentation/ui/statisticsdetail/StatisticsDetailActivity.kt
+++ app/src/main/java/com/inari/team/presentation/ui/statisticsdetail/StatisticsDetailActivity.kt
@@ -128,7 +128,6 @@ class StatisticsDetailActivity : BaseActivity(), GnssEventsListener {
                     it.axisLeft.axisMinimum = 0f
                     it.axisRight.isEnabled = false
 
-
                     val lp = RelativeLayout.LayoutParams(
                         RelativeLayout.LayoutParams.MATCH_PARENT,
                         RelativeLayout.LayoutParams.MATCH_PARENT
