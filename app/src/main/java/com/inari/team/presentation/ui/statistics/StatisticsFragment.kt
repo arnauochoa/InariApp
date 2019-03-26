24c3b0da33a96c5be7de9e4a423e0713b00704fa
arnauochoa <arnau.ochoa@gmail.com>
2019-03-26 11:40:23 +0100

    graph tests

--- app/src/main/java/com/inari/team/presentation/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/inari/team/presentation/ui/statistics/StatisticsFragment.kt
@@ -51,7 +51,7 @@ class StatisticsFragment : BaseFragment(), View.OnClickListener {
             when (view.id) {
 
                 R.id.cardRMS -> {
-                    i.putExtra(StatisticsDetailActivity.GRAPH_TYPE, StatisticsDetailActivity.RMS)
+                    i.putExtra(StatisticsDetailActivity.GRAPH_TYPE, StatisticsDetailActivity.ELEVATION_CNO)
                 }
                 R.id.cardCNO -> {
                     i.putExtra(StatisticsDetailActivity.GRAPH_TYPE, StatisticsDetailActivity.CNO)
