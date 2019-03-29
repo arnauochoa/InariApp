d6f10858dd2d282043576e23874598b7c25f55cd
arnauochoa <arnau.ochoa@gmail.com>
2019-03-29 13:20:21 +0100

    control commit

--- app/src/main/java/com/inari/team/presentation/ui/about/AboutFragment.kt
+++ app/src/main/java/com/inari/team/presentation/ui/about/AboutFragment.kt
@@ -1,6 +1,7 @@
 package com.inari.team.presentation.ui.about
 
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -8,14 +9,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.inari.team.BuildConfig
-import com.inari.team.R
 import com.inari.team.core.base.BaseFragment
 import com.inari.team.core.navigator.Navigator
 import kotlinx.android.synthetic.main.fragment_about.*
 import javax.inject.Inject
-import android.content.ActivityNotFoundException
-
-
 
 
 class AboutFragment : BaseFragment() {
