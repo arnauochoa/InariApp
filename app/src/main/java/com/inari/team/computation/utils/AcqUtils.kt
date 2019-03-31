f8069e9cabfc09fc31d3d42a680ece851ec3183e
albertbecervas <albertbecerrahervas@gmail.com>
2019-03-31 19:56:51 +0200

    finished adquiring Information

--- app/src/main/java/com/inari/team/computation/utils/AcqUtils.kt
+++ app/src/main/java/com/inari/team/computation/utils/AcqUtils.kt
@@ -1,6 +1,46 @@
 package com.inari.team.computation.utils
 
+import android.location.GnssMeasurement
 import com.inari.team.computation.Constants.C
+import com.inari.team.computation.converters.applyMod
+import com.inari.team.computation.data.AcqInformationMeasurements
+import com.inari.team.computation.data.GAL_E1C
+import com.inari.team.computation.data.Satellite
+import com.inari.team.computation.data.WEEK_NANOS
+
+fun getTowDecodeSatellite(meas: GnssMeasurement, acqInformationMeasurements: AcqInformationMeasurements): Satellite {
+    return with(meas) {
+        val tTx = getTtx(timeOffsetNanos, receivedSvTimeNanos)
+        val tRx = applyMod(acqInformationMeasurements.timeNanosGnss, WEEK_NANOS)
+        Satellite(
+            svid = svid,
+            state = state,
+            multiPath = multipathIndicator,
+            carrierFreq = carrierFrequencyHz.toDouble(),
+            tTx = tTx,
+            tRx = tRx,
+            cn0 = cn0DbHz,
+            pR = getPseudoRange(tTx, tRx)
+        )
+    }
+}
+
+fun getE1CSatellite(meas: GnssMeasurement, acqInformationMeasurements: AcqInformationMeasurements): Satellite {
+    return with(meas) {
+        val tTx = getTtx(timeOffsetNanos, receivedSvTimeNanos)
+        val tRx = acqInformationMeasurements.timeNanosGnss
+        Satellite(
+            svid = svid,
+            state = state,
+            multiPath = multipathIndicator,
+            carrierFreq = carrierFrequencyHz.toDouble(),
+            tTx = tTx,
+            tRx = tRx,
+            cn0 = cn0DbHz,
+            pR = getPseudoRange(applyMod(tTx, GAL_E1C), applyMod(tRx, GAL_E1C))
+        )
+    }
+}
 
 fun checkTowDecode(tow: Int): Boolean {
 
