49a2e43ed49c555711b4f1e02c9a45ecebf7efcc
albertbecervas <albertbecerrahervas@gmail.com>
2019-04-04 17:44:51 +0200

    comment test

--- app/src/test/java/com/inari/team/PvtUnitTest.kt
+++ app/src/test/java/com/inari/team/PvtUnitTest.kt
@@ -19,31 +19,51 @@ class PvtUnitTest {
     @Test
     fun pvt_isCorrect() {
 
-        val fileInput = this.javaClass.classLoader?.getResourceAsStream("badia.txt")
-        val acqInfoString = fileInput?.bufferedReader().use { it?.readText() }
+        //list of files that we want to compute
+        val filesList = arrayListOf("badia.txt", "badia.txt")
 
-        val type = object : TypeToken<AcqInformation>() {}.type
+        //for each file added in the list, compute the position
+        filesList.forEach { fileName ->
 
-        val acqInfo = Gson().fromJson<AcqInformation>(acqInfoString, type)
+            //Getting the file
+            val fileInput = this.javaClass.classLoader?.getResourceAsStream(fileName)
+            val acqInfoString = fileInput?.bufferedReader().use { it?.readText() }
+            val type = object : TypeToken<AcqInformation>() {}.type
 
-        print("Reference Position: ${acqInfo.refLocation.refLocationLla} \n")
+            //parse data into the object AcqInformation
+            val acqInfo = Gson().fromJson<AcqInformation>(acqInfoString, type)
 
-        val computedPositions = testComputePvt(acqInfo)
+            //print the reference position from google in order to know the approximate position tha we should be getting
+            print(
+                "====================================================\n" +
+                        "Reference Position: ${acqInfo.refLocation.refLocationLla.latitude}," +
+                        " ${acqInfo.refLocation.refLocationLla.longitude}\n"
+            )
 
-        if (computedPositions.isNotEmpty()) {
-            computedPositions.forEach {
-                print("${it.modeName} --> Computed Position:${it.compPosition}")
+            //start the computation
+            val computedPositions = testComputePvt(acqInfo)
+
+            //print the results obtained or error
+            if (computedPositions.isNotEmpty()) {
+                computedPositions.forEach {
+                    print("${it.modeName} --> Computed Position:${it.compPosition}")
+                }
+            } else {
+                print("Could not compute any positions")
             }
-        } else {
-            print("Could not compute any positions")
+
         }
+
+
     }
 
     private fun testComputePvt(acq: AcqInformation): List<ResponsePvtMode> {
 
+        //initialize an empty array of responses, we should get one response of every selected mode
         val responses = arrayListOf<ResponsePvtMode>()
 
         var acqInformation = acq
+
         //CN0 mask
         if (acqInformation.cn0mask != 0) {
             acqInformation = applyMask(acqInformation, Constants.CN0_MASK)
@@ -53,14 +73,17 @@ class PvtUnitTest {
             acqInformation = applyMask(acqInformation, Constants.ELEVATION_MASK)
         }
 
+        //for each mode selected
         acqInformation.modes.forEach {
 
+            //obtain pvt
             val pvtMultiConst = try {
                 pvtMultiConst(acqInformation, it)
             } catch (e: Exception) {
                 ResponsePvtMultiConst()
             }
 
+            //if pvt is valid add it to responses list
             if (pvtMultiConst.pvt.lat in -180.0..180.0 && pvtMultiConst.pvt.lng in -180.0..180.0) {
 
                 val pvtResponse = ResponsePvtMode(
@@ -76,9 +99,8 @@ class PvtUnitTest {
 
                 responses.add(pvtResponse)
             }
-
-
         }
+
         return responses
     }
 }
