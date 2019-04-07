c00e9398f1883b8015ed9db6e12f730c757cc659
albertbecervas <albertbecerrahervas@gmail.com>
2019-04-07 22:56:19 +0200

    implement loggers

--- app/src/main/java/com/inari/team/computation/PvtEngine.kt
+++ app/src/main/java/com/inari/team/computation/PvtEngine.kt
@@ -1,17 +1,12 @@
 package com.inari.team.computation
 
 import com.google.android.gms.maps.model.LatLng
-import com.google.gson.Gson
 import com.inari.team.computation.data.ResponsePvtMultiConst
 import com.inari.team.computation.infoextractors.getAcqInfo
 import com.inari.team.computation.utils.Constants.CN0_MASK
 import com.inari.team.computation.utils.Constants.ELEVATION_MASK
-import com.inari.team.core.utils.saveGnssFile
 import com.inari.team.presentation.model.GnssData
 import com.inari.team.presentation.model.ResponsePvtMode
-import okhttp3.MediaType
-import okhttp3.ResponseBody
-import java.util.*
 
 fun computePvt(gnssData: GnssData): List<ResponsePvtMode> {
 
@@ -19,9 +14,6 @@ fun computePvt(gnssData: GnssData): List<ResponsePvtMode> {
 
     //Obtain Information
     var acqInformation = getAcqInfo(gnssData)
-
-    //todo remove after testing
-    val savedAcqInformationLog = acqInformation
 
     //CN0 mask
     if (acqInformation.cn0mask != 0) {
@@ -38,15 +30,6 @@ fun computePvt(gnssData: GnssData): List<ResponsePvtMode> {
             pvtMultiConst(acqInformation, it)
         } catch (e: Exception) {
             ResponsePvtMultiConst()
-        }
-
-        //todo remove after testing
-        try {
-
-            val acq = Gson().toJson(savedAcqInformationLog)
-            saveGnssFile("${Date()}.txt", ResponseBody.create(MediaType.parse("text/plain"), acq))
-        } catch (e: Exception) {
-
         }
 
         if (pvtMultiConst.pvt.lat in -180.0..180.0 && pvtMultiConst.pvt.lng in -180.0..180.0) {
