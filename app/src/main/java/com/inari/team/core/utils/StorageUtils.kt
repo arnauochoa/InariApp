0ad3dd053342ea9736e449f35b9d2e33af6ed8a1
albertbecervas <albertbecerrahervas@gmail.com>
2019-04-07 23:16:24 +0200

    add position logs

--- app/src/main/java/com/inari/team/core/utils/StorageUtils.kt
+++ app/src/main/java/com/inari/team/core/utils/StorageUtils.kt
@@ -14,7 +14,7 @@ const val APP_ROOT: String = "/Inari/Logs/Nmea/"
 const val POSITION_ROOT: String = "/Inari/Positions/"
 
 //used for post processing
-const val GNSS_ROOT: String = "/Inari/Gnss/"
+//const val GNSS_ROOT: String = "/Inari/Gnss/"
 
 @SuppressLint("SetWorldReadable")
 fun savePositionFile(url: String, responseBody: ResponseBody) {
@@ -56,40 +56,40 @@ fun savePositionFile(url: String, responseBody: ResponseBody) {
 }
 
 //used for post processing
-@SuppressLint("SetWorldReadable")
-fun saveGnssFile(url: String, responseBody: ResponseBody) {
-    val dir =
-        File(root.absolutePath + GNSS_ROOT)
-    dir.mkdirs()
-
-    val file = File(dir, url)
-    file.setReadable(true, false)
-
-    var inputStream: InputStream? = null
-    var outputStream: OutputStream? = null
-
-    try {
-        val fileReader = ByteArray(4096)
-
-        inputStream = responseBody.byteStream()
-        outputStream = FileOutputStream(file)
-
-        while (true) {
-            val read = inputStream!!.read(fileReader)
-
-            if (read == -1) {
-                break
-            }
-            outputStream.write(fileReader, 0, read)
-        }
-        outputStream.flush()
-
-    } catch (e: IOException) {
-    } finally {
-        inputStream?.close()
-        outputStream?.close()
-    }
-}
+//@SuppressLint("SetWorldReadable")
+//fun saveGnssFile(url: String, responseBody: ResponseBody) {
+//    val dir =
+//        File(root.absolutePath + GNSS_ROOT)
+//    dir.mkdirs()
+//
+//    val file = File(dir, url)
+//    file.setReadable(true, false)
+//
+//    var inputStream: InputStream? = null
+//    var outputStream: OutputStream? = null
+//
+//    try {
+//        val fileReader = ByteArray(4096)
+//
+//        inputStream = responseBody.byteStream()
+//        outputStream = FileOutputStream(file)
+//
+//        while (true) {
+//            val read = inputStream!!.read(fileReader)
+//
+//            if (read == -1) {
+//                break
+//            }
+//            outputStream.write(fileReader, 0, read)
+//        }
+//        outputStream.flush()
+//
+//    } catch (e: IOException) {
+//    } finally {
+//        inputStream?.close()
+//        outputStream?.close()
+//    }
+//}
 
 
 fun getFile(fileName: String): File = File(root.absolutePath + APP_ROOT + fileName)
